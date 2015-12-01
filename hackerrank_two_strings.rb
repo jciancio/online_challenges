2edeb718610c715d8d53ea1495ef648b8bac8726
jciancio <jciancio7@gmail.com>
2015-12-01 17:20:45 -0500

    algorithim works with test input

---
+++ hackerrank_two_strings.rb
@@ -0,0 +1,20 @@
+t = gets.strip.to_i
+
+test_strings = []
+
+for a0 in (0..t-1)
+    a = gets.chomp.to_s.downcase
+    b = gets.chomp.to_s.downcase
+    test_strings.push [a, b]
+end
+
+for pair in test_strings do
+  first_string = pair.first.chars.to_a
+  second_string = pair.last.chars.to_a
+  intersection = first_string & second_string
+  if intersection.empty?
+    puts 'NO'
+  else
+    puts 'YES'
+  end
+end
