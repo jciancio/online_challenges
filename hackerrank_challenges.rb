e2356985bc858b3fa7643a8304f9417a928c244a
jciancio <jciancio7@gmail.com>
2015-12-01 15:41:19 -0500

    program should be working, accounted for zeros, using sample input, i return correct result

--- hackerrank_challenges.rb
+++ hackerrank_challenges.rb
@@ -1,7 +1,6 @@
 t = gets.strip.to_i
 
 integers = []
-digits = []
 results = []
 
 for a0 in (0..t-1)
@@ -9,23 +8,17 @@ for a0 in (0..t-1)
     integers << n
 end
 
-
 for integer in integers do
   j = 0
+  digits = []
   digits << integer.to_s.chars.map(&:to_i)
   digits.each do |i|
     for digit in i do
-      if integer % digit == 0
+      if (digit || integer) == 0
+      elsif integer % digit == 0
         j += 1
       end
     end
   end
   p j
 end
-
-# digits << integer.to_s.chars.map!(&:to_i)
-# for digit in digits do
-#   if integer % digit.to_i == 0
-#     p 1
-#   end
-# end
