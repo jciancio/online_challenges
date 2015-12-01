5c67125c0483016a14aabba1804e7b88b79ae9c8
jciancio <jciancio7@gmail.com>
2015-12-01 18:55:22 -0500

    algorithim working fine locally, not when submitted to hackerrank

--- hackerrank_sherlock_squares.rb
+++ hackerrank_sherlock_squares.rb
@@ -3,9 +3,17 @@ t = gets.strip.to_i
 test_numbers = []
 
 for a0 in (0..t-1)
-    a = gets.strip.to_i
-    b = gets.strip.to_i
+    a = gets.to_i
+    b = gets.to_i
     test_numbers.push [a, b]
 end
 
-p test_numbers
+for pair in test_numbers do
+  j = 0
+  (pair.first..pair.last).each do |i|
+    if Math.sqrt(i) % 1 == 0
+      j += 1
+    end
+  end
+  p j
+end
