01a4c04dcf9c9ed16d1ecc88c36c94b83ec8bcde
jciancio <jciancio7@gmail.com>
2015-12-03 20:40:05 -0500

    no output is as test expects

--- hackerrank_quick_sort1.rb
+++ hackerrank_quick_sort1.rb
@@ -2,6 +2,9 @@ def partition(ar)
   left = ar.select { |x| x<ar[0]}
   right = ar.select { |x| x>=ar[0]}
   result = left + right
+  result.each do |x|
+    print x, " "
+  end
 end
 count = gets.to_i;
 ar = STDIN.gets.chomp.split(" ").map{|x|x.to_i};
