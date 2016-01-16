72ee1b79879f552207b028f88c0782e8dfde93fa
jciancio <jciancio7@gmail.com>
2016-01-16 17:36:43 -0500

    got the method working, am now refactoring

--- credit-card-mask.rb
+++ credit-card-mask.rb
@@ -1,5 +1,14 @@
 # This method masks the last four characters of the given string
 
 def maskify(cc)
-
+  n = 0
+  if cc.length > 4
+    n += cc.size - 4
+    n.times do |i|
+      cc[i] = '#'
+    end
+    return cc
+  else
+    return cc
+  end
 end
