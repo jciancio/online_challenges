5466101e197db3c498f4621c283c85ead5448c39
jciancio <jciancio7@gmail.com>
2016-01-18 21:44:42 -0500

    rewrote function to work better with negative numbers

--- triangular-treasure.rb
+++ triangular-treasure.rb
@@ -1,14 +1,18 @@
 # Function Returns the nth Triangular Number
 
 def triangular( n )
-  if n < 0
-    0
+  if n > 0
+    x = 0
+    z = 1
+    until z == (n + 1) do
+      x += z
+      z += 1
+    end
+    p x
+  else p 0
   end
-  x = 0
-  z = 1
-  until z == (n + 1) do
-    x += z
-    z += 1
-  end
-  p x
 end
+
+triangular(-10)
+triangular(4)
+triangular(-4)
